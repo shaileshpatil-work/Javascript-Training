5be09f50b2b71f87075e4ca8c8d52546c8f58a18
abp437 <abp437@gmail.com>
2017-12-03 21:37:33 +0530

    New Components Added

--- everscriptsSite/everscripts/src/app/header/header.component.ts
+++ everscriptsSite/everscripts/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-header',
@@ -10,6 +11,9 @@ export class HeaderComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(window).scroll(function () {
+      $('.something').css('background-color', 'yellow');
+    });
   }
 
 }
